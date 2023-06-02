0703cfc0173bb0a6fd87382c12b703e72101cff1
Felipe Aros <slimerboy9f@gmail.com>
2023-06-02 10:44:35 -0400

    arreglo docker

--- client/src/Result.jsx
+++ client/src/Result.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ExportToExcel from './Excel';
 import CurrencyFormat from 'react-currency-format';
 import {useState} from 'react'
-import axios from 'axios'
+
 
 
 const Result = (props) => {
 
   const [jsonData, setJsonData] = useState([]);
-  var vars = [];
   const fetchData = () => {
     fetch('http://localhost:8000/select')
       .then(response => response.json())
@@ -27,7 +26,7 @@ const Result = (props) => {
         <label>valor moneda <CurrencyFormat value={props.ufPrice} displayType={'text'} thousandSeparator={true} prefix={'$'} /></label><br/>
         <label>Monto: <CurrencyFormat value={props.totalAmount} displayType={'text'} thousandSeparator={true} prefix={'$'} /></label><br/>
         {
-          window.localStorage.getItem('SessionType')==="admin" ? <ExportToExcel jsonData={jsonData} fileName="data"/>:<></>
+          window.localStorage.getItem('SessionType')==="admin" ? <ExportToExcel jsonData={jsonData} fileName="Registro-Datos"/>:<></>
 
        
         }
